dcc995e978bb98283edb6440c2c7c7d8c6c22634
kai-rcc <kai-rcc@gmx.de>
2016-11-06 01:56:57 +0100

    initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,19 @@
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.params.TestNet3Params;
+
+
+
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//initialize neccessary bitcoinj variables
+		final NetworkParameters params = TestNet3Params.get();
+		int proof_of_burn;
+
+	}
+
+}
